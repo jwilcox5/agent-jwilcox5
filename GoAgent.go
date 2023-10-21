418eab1763a4e5c79453dd37dcba3baccc4b9579
jwilcox5 <123770450+jwilcox5@users.noreply.github.com>
2023-10-21 18:45:51 -0400

    Update GoAgent.go

--- GoAgent.go
+++ GoAgent.go
@@ -2,18 +2,26 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
-	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"strconv"
 	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+
 	"github.com/jamespearly/loggly"
 )
 
-type AirQuality struct {
-	Status string `json:"status"`
-	Data   struct {
+type AirQualityData struct {
+	DateTime string `json:"datetime"`
+	Status   string `json:"status"`
+	Data     struct {
 		City     string `json:"city"`
 		State    string `json:"state"`
 		Country  string `json:"country"`
@@ -44,14 +52,18 @@ type AirQuality struct {
 
 func main() {
 
-	var aqData AirQuality
+	awsSess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
 
-	ticker := time.NewTicker(5 * time.Minute)
+	svc := dynamodb.New(awsSess)
+
+	pollingInterval := flag.Int("interval", 1, "how frequent will API requests be made, in minutes")
+	flag.Parse()
+	ticker := time.NewTicker(time.Minute * time.Duration(*pollingInterval))
 
 	for {
-
-		time := <-ticker.C
-		fmt.Println(time)
+		<-ticker.C
 
 		request, reqErr := http.Get("http://api.airvisual.com/v2/city?city=Sacramento&state=California&country=USA&key=9f5d9c77-3aaa-44e0-98c3-a24e67884a93")
 
@@ -59,32 +71,51 @@ func main() {
 			panic(reqErr)
 		}
 
-		requestData, reqDataErr := io.ReadAll(request.Body)
+		requestData, reqDataErr := ioutil.ReadAll(request.Body)
 
 		if reqDataErr != nil {
 			panic(reqDataErr)
 		}
 
-		Data := []byte(requestData)
+		var aqData AirQualityData
 
-		jsonErr := json.Unmarshal(Data, &aqData)
+		aqData.DateTime = time.Now().Format(time.RFC3339)
+		jsonErr := json.Unmarshal(requestData, &aqData)
 
 		if jsonErr != nil {
 			panic(jsonErr)
 		}
 
-		var tag string
-		tag = "IQAir"
+		av, dbErr := dynamodbattribute.MarshalMap(aqData)
 
-		client := loggly.New(tag)
+		if dbErr != nil {
+			log.Fatalf("Got error marshalling map: %s", dbErr)
+		}
 
-		err := client.EchoSend("info", "Status: "+aqData.Status+"\nCity: "+aqData.Data.City+"\nState: "+aqData.Data.State+"\nCountry: "+aqData.Data.Country+"\nType: "+aqData.Data.Location.Type+"\nCoordinates: "+strconv.FormatFloat(aqData.Data.Location.Coordinates[0], 'E', -1, 64)+", "+strconv.FormatFloat(aqData.Data.Location.Coordinates[1], 'E', -1, 64)+
+		tableName := "air-quality-data-jwilcox5"
+
+		input := &dynamodb.PutItemInput{
+			Item:      av,
+			TableName: aws.String(tableName),
+		}
+
+		_, putErr := svc.PutItem(input)
+
+		if putErr != nil {
+			log.Fatalf("Got error calling PutItem: %s", putErr)
+		}
+
+		logTag := "IQAir Air Quality Data"
+
+		client := loggly.New(logTag)
+
+		logErr := client.EchoSend("info", "\nData Size: "+strconv.FormatInt(request.ContentLength, 10)+"\nTime: "+aqData.DateTime+"\nStatus: "+aqData.Status+"\nCity: "+aqData.Data.City+"\nState: "+aqData.Data.State+"\nCountry: "+aqData.Data.Country+"\nType: "+aqData.Data.Location.Type+"\nCoordinates: "+strconv.FormatFloat(aqData.Data.Location.Coordinates[0], 'E', -1, 64)+", "+strconv.FormatFloat(aqData.Data.Location.Coordinates[1], 'E', -1, 64)+
 			"\nTimestamp: "+aqData.Data.Current.Pollution.Ts.String()+"\nAQI US: "+strconv.Itoa(aqData.Data.Current.Pollution.Aqius)+
 			"\nMain Pollutant US: "+aqData.Data.Current.Pollution.Mainus+"\nAQI China: "+strconv.Itoa(aqData.Data.Current.Pollution.Aqicn)+
 			"\nMain Pollutant China: "+aqData.Data.Current.Pollution.Maincn+"\nTimestamp: "+aqData.Data.Current.Weather.Ts.String()+
 			"\nTemperature: "+strconv.Itoa(aqData.Data.Current.Weather.Tp)+"\nAir Pressure: "+strconv.Itoa(aqData.Data.Current.Weather.Pr)+
 			"\nHumidity: "+strconv.Itoa(aqData.Data.Current.Weather.Hu)+"\nWind Speed: "+strconv.FormatFloat(aqData.Data.Current.Weather.Ws, 'E', -1, 64)+
 			"\nWind Direction: "+strconv.Itoa(aqData.Data.Current.Weather.Wd)+"\nWeather Icon Code: "+aqData.Data.Current.Weather.Ic)
-		fmt.Println("err:", err)
+		fmt.Println("err:", logErr)
 	}
 }
